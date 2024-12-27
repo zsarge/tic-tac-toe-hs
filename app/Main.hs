999b5ce60b75928dbe117a28a3865eef80ebefd2
Zack Sargent <zack@zack.fyi>
2024-12-27 04:27:05 -0500

    index 1-9

--- app/Main.hs
+++ app/Main.hs
@@ -7,8 +7,18 @@ import Data.List.Split (chunksOf)
 import GHC.TypeLits (Nat)
 import Text.Printf
 
+newtype Index = UnsafeIndex { i :: Int } deriving (Eq)
+
+instance Show Index where 
+    show (UnsafeIndex index) = show index
+
+makeIndex :: Int -> Maybe Index
+makeIndex i
+  | 1 <= i && i <= 9 = Just (UnsafeIndex i)
+  | otherwise = Nothing
+
 data Player = X | O deriving (Show, Eq)
-data Cell = Player Player | Num Int deriving (Show, Eq)
+data Cell = Player Player | Num Index deriving (Show, Eq)
 
 toString :: Cell -> String
 toString (Player X) = "X"
@@ -20,29 +30,24 @@ type Board = Vector 9 Cell
 
 -- "@9" refers to the type-level number 9
 newBoard :: Board
-newBoard = generate @9 (\x -> Num (fromIntegral x))
-
-newtype Index = UnsafeMakeIndex { i :: Int }
-makeIndex :: Int -> Maybe Index
-makeIndex i
-  | 1 <= i && i <= 9 = Just (UnsafeMakeIndex i)
-  | otherwise = Nothing
+newBoard = generate @9 (\x -> Num (UnsafeIndex $ (fromIntegral x) + 1))
 
 move :: Board -> Index -> Player -> Board
 move board index player = newBoard
 
 showBoard :: Board -> String
 showBoard board = formatted
-  where allData = (map toString . toList) board
-        chunked = chunksOf 3 allData
+  where -- create 2d array of length-1 strings
+        chunked = chunksOf 3 ((map toString . toList) board)
+        -- seperate the numbers by pipes, padding with space and newline
         rows = map ((++ "\n") . (" " ++) . intercalate " | ") chunked 
-        formatted = intercalate divider rows
         divider = "---+---+---\n"
+        formatted = intercalate divider rows
 
 main :: IO ()
 main = do
   -- let board = newBoard
-  -- let index = UnsafeMakeIndex 0
+  -- let index = UnsafeIndex 0
   -- print $ move board index X
   putStrLn $ showBoard newBoard
 
